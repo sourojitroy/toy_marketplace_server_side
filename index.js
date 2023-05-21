a14bdebad4d8b8ba7d201ba4aad902787dea0102
sourojitroy <roy.souro2013@gmail.com>
2023-05-21 21:47:18 +0600

    collection added

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ app.use(cors())
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2lbftot.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const productCollection = client.db("toyCar").collection("products")
