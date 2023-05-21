cfad9836c7c60c7cf74f89db29ea49601c34c204
sourojitroy <roy.souro2013@gmail.com>
2023-05-21 21:46:05 +0600

    firebase access added

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2lbftot.mongodb.net/?retryWrites=true&w=majority`;
